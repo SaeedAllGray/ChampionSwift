51b3a95a1aa2e27362fc9666036a9c517506daf5
SaeedAllGray <saeedag.imail@gmail.com>
2021-07-28 10:33:47 +0430

    Initial Commit

---
+++ ChampionSwift/ChampionSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChampionSwiftApp.swift
+//  ChampionSwift
+//
+//  Created by saeed on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ChampionSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
